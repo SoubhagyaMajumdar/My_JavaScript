9ff289cc3a76a2f8c546ec311f0af7efbe10ec92
Soubhagya Majumdar <155235637+SoubhagyaMajumdar@users.noreply.github.com>
2025-09-01 17:31:17 +0000

    datatypes

---
+++ JS_Basics/Important_1.js
@@ -0,0 +1,11 @@
+"use strict";   // treat all JS Code as "New" version
+
+// alert("Hello")       <--- Error:  Because, we are using Node.js (not Browser)
+
+
+
+console.log("Hi");  console.log("Hello")         // Here, we have to use (;)
+
+
+console.log("Her")      // Here, we Don't need to use (;)
+console.log("His")
---
+++ JS_Basics/TypeOf.js
@@ -0,0 +1,16 @@
+let A = 10
+let B = "Hello"
+let C;
+let D = null;
+let E = undefined;
+
+Apple = 20;
+
+console.log(typeof A)
+console.log(typeof B)
+console.log(typeof C)
+console.log(typeof D)
+console.log(typeof E)
+console.log(typeof Apple)
+
+console.table([typeof A, typeof B, typeof C, typeof D, typeof E, typeof Apple])
