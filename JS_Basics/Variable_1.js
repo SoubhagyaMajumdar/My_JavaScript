3cea4557d130ae04ff8c89045188cea64fd36392
Soubhagya Majumdar <155235637+SoubhagyaMajumdar@users.noreply.github.com>
2025-09-01 17:31:40 +0000

    important

---
+++ JS_Basics/Variable_1.js
@@ -0,0 +1,13 @@
+const Id = 101
+
+let Name = "Rohan"
+
+var Password = "1234"
+
+let Good = true     //boolean
+
+City = "Jaipur"
+
+let Money;     // No value assigned (Output: undefined)
+
+console.table([Id, Name, Password, City, Money, Good])
